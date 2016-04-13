7de889ae67fa5d66b321f8f628f9fce54718bf27
justinrule99 <justinrule99@gmail.com>
2016-04-12 19:40:16 -0500

    quick

--- Debate Website/js/login.js
+++ Debate Website/js/login.js
@@ -2,6 +2,8 @@ console.log("did i do this right");
 
 function onSignUp(){
 	console.log("you signed up");
+	var fullname = prompt("What is your full name?");
+	var password = prompt("Choose a password");
 }
 
 function onLogIn(){
